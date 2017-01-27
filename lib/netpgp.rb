63664f8bb5b3d8544428e62eed5bc8dff666fedf
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-01-27 12:11:42 -0500

    Move all low-level code to bindings/ directory.

--- lib/netpgp.rb
+++ lib/netpgp.rb
@@ -1,5 +1,5 @@
-require 'netpgp/libnetpgp'
-require 'netpgp/libc'
-require 'netpgp/libopenssl'
-require 'netpgp/dynarray'
+require 'netpgp/bindings/libnetpgp'
+require 'netpgp/bindings/libc'
+require 'netpgp/bindings/libopenssl'
+require 'netpgp/bindings/dynarray'
 
