f4847a18489a6f741d8e4305e6daf0e1c4d1bad8
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-02-02 16:47:57 -0500

    Add a number of functions to support signing/verifying.

--- lib/netpgp/bindings/libnetpgp.rb
+++ lib/netpgp/bindings/libnetpgp.rb
@@ -49,5 +49,92 @@ module LibNetPGP
   attach_function :pgp_add_selfsigned_userid,
                   [PGPKey.by_ref, :string],
                   :uint
+
+  attach_function :pgp_keyring_free,
+                  [:pointer],
+                  :void
+
+  attach_function :pgp_keyring_fileread,
+                  [PGPKeyring.by_ref, :uint, :string],
+                  :uint
+
+  attach_function :pgp_sign_file,
+                  [PGPIO.by_ref, :string, :string, PGPSecKey.by_ref, :string, :int64, :uint64, :uint, :uint, :uint],
+                  :uint
+  attach_function :pgp_sign_detached,
+                  [PGPIO.by_ref, :string, :string, PGPSecKey.by_ref, :string, :int64, :uint64, :uint, :uint],
+                  :int
+  attach_function :pgp_sign_buf,
+                  [PGPIO.by_ref, :pointer, :size_t, PGPSecKey.by_ref, :int64, :uint64, :string, :uint, :uint],
+                  PGPMemory.by_ref
+  attach_function :pgp_validate_file,
+                  [PGPIO.by_ref, PGPValidation.by_ref, :string, :string, :int, PGPKeyring.by_ref],
+                  :uint
+  attach_function :pgp_validate_mem,
+                  [PGPIO.by_ref, PGPValidation.by_ref, PGPMemory.by_ref, :pointer, :int, PGPKeyring.by_ref],
+                  :uint
+
+  attach_function :pgp_encrypt_file,
+                  [PGPIO.by_ref, :string, :string, PGPKey.by_ref, :uint, :uint, :string],
+                  :uint
+  attach_function :pgp_encrypt_buf,
+                  [PGPIO.by_ref, :pointer, :size_t, PGPKey.by_ref, :uint, :string],
+                  :pointer
+
+  attach_function :pgp_export_key,
+                  [PGPIO.by_ref, PGPKey.by_ref, :string],
+                  :string
+
+  attach_function :pgp_memory_new,
+                  [],
+                  PGPMemory.by_ref
+  attach_function :pgp_memory_free,
+                  [PGPMemory.by_ref],
+                  :void
+  attach_function :pgp_memory_init,
+                  [PGPMemory.by_ref, :size_t],
+                  :void
+  attach_function :pgp_memory_pad,
+                  [PGPMemory.by_ref, :size_t],
+                  :void
+  attach_function :pgp_memory_add,
+                  [PGPMemory.by_ref, :pointer, :size_t],
+                  :void
+  attach_function :pgp_memory_place_int,
+                  [PGPMemory.by_ref, :uint, :uint, :size_t],
+                  :void
+  attach_function :pgp_memory_make_packet,
+                  [PGPMemory.by_ref, :pgp_content_enum],
+                  :void
+  attach_function :pgp_memory_clear,
+                  [PGPMemory.by_ref],
+                  :void
+  attach_function :pgp_memory_release,
+                  [PGPMemory.by_ref],
+                  :void
+  attach_function :pgp_mem_len,
+                  [PGPMemory.by_ref],
+                  :size_t
+  attach_function :pgp_mem_data,
+                  [PGPMemory.by_ref],
+                  :pointer
+  attach_function :pgp_mem_readfile,
+                  [PGPMemory.by_ref, :string],
+                  :int
+
+  attach_function :pgp_is_key_secret,
+                  [PGPKey.by_ref],
+                  :uint
+  attach_function :pgp_get_seckey,
+                  [PGPKey.by_ref],
+                  :pointer
+  attach_function :pgp_decrypt_seckey,
+                  [PGPKey.by_ref, :pointer],
+                  :pointer
+
+  attach_function :pgp_stream_delete,
+                  [:pointer],
+                  :void
+
 end
 
