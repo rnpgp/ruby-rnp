e21d03d1346c4963718e826f3fbad7ba354af70e
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-02-20 13:34:42 -0500

    Move bindings to 'lowlevel' directory.

---
+++ lib/netpgp/lowlevel.rb
@@ -0,0 +1,6 @@
+require_relative 'lowlevel/libnetpgp'
+require_relative 'lowlevel/libc'
+require_relative 'lowlevel/libopenssl'
+require_relative 'lowlevel/dynarray'
+require_relative 'lowlevel/utils'
+
