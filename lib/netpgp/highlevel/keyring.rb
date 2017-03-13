4710cbb38970c0eaaec65f1bcc1837807ea43529
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-03-13 19:45:14 -0400

    Add public key exporting with subkeys. Work in progress.

--- lib/netpgp/highlevel/keyring.rb
+++ lib/netpgp/highlevel/keyring.rb
@@ -29,6 +29,86 @@ class Keyring
     NetPGP::verify(@keys, data, armored)
   end
 
+  # Export a specific key
+  def export_key(key, armored=true)
+    case
+    when key.is_a?(PublicKey)
+      export_public_key(key, armored)
+    when key.is_a?(SecretKey)
+    end
+  end
+
+  def export_public_key(key, armored=true)
+    # no exporting subkeys directly
+    raise if @parent
+    keyid = key.key_id
+    # we'll need the corresponding secret key for signatures
+    seckey = secret_keys.find {|key| key.key_id == keyid}
+    return nil if not seckey
+    output_ptr = FFI::MemoryPointer.new(:pointer)
+    mem_ptr = FFI::MemoryPointer.new(:pointer)
+    output = nil
+    mem = nil
+    begin
+      LibNetPGP::pgp_setup_memory_write(output_ptr, mem_ptr, 4096)
+      output = LibNetPGP::PGPOutput.new(output_ptr.read_pointer)
+      mem = LibNetPGP::PGPMemory.new(mem_ptr.read_pointer)
+      native = LibNetPGP::PGPKey.new
+      key.to_native_key(native)
+      if seckey
+        decrypted_seckey = seckey.decrypted_seckey
+        if decrypted_seckey
+          seckey = SecretKey.from_native(decrypted_seckey)
+          seckey.to_native(native[:key][:seckey])
+          native[:type] = :PGP_PTAG_CT_SECRET_KEY
+        end
+      end
+      LibNetPGP::dynarray_clear(native, 'uid', :string)
+      key.userids.each {|userid|
+        LibNetPGP::pgp_add_selfsigned_userid(native, userid)
+      }
+      subkeysring_mem = LibC::calloc(1, LibNetPGP::PGPKeyring.size)
+      subkeysring = LibNetPGP::PGPKeyring.new(subkeysring_mem)
+      NetPGP::keys_to_native_keyring(key.subkeys, subkeysring)
+      # add a binding signature to each subkey
+      (0..LibNetPGP::dynarray_count(subkeysring, 'key') - 1).each {|n|
+        subkey = LibNetPGP::dynarray_get_item(subkeysring, 'key', LibNetPGP::PGPKey, n)
+        LibNetPGP::dynarray_clear(subkey, 'packet', LibNetPGP::PGPSubPacket)
+        sig = nil
+        sigoutput = nil
+        mem_sig = nil
+        begin
+          sig = LibNetPGP::pgp_create_sig_new
+          LibNetPGP::pgp_sig_start_subkey_sig(sig, native[:key][:pubkey], subkey[:key][:pubkey], :PGP_SIG_SUBKEY)
+          LibNetPGP::pgp_add_time(sig, subkey[:key][:pubkey][:birthtime], 'birth')
+          # TODO expiration
+          LibNetPGP::pgp_add_issuer_keyid(sig, native[:sigid])
+          LibNetPGP::pgp_end_hashed_subpkts(sig)
+
+          sigoutput_ptr = FFI::MemoryPointer.new(:pointer)
+          mem_sig_ptr = FFI::MemoryPointer.new(:pointer)
+          LibNetPGP::pgp_setup_memory_write(sigoutput_ptr, mem_sig_ptr, 128)
+          sigoutput = LibNetPGP::PGPOutput.new(sigoutput_ptr.read_pointer)
+          LibNetPGP::pgp_write_sig(sigoutput, sig, native[:key][:pubkey], native[:key][:seckey])
+          mem_sig = LibNetPGP::PGPMemory.new(mem_sig_ptr.read_pointer)
+          sigpkt = LibNetPGP::PGPSubPacket.new
+          sigpkt[:length] = LibNetPGP::pgp_mem_len(mem_sig)
+          sigpkt[:raw] = LibNetPGP::pgp_mem_data(mem_sig)
+          LibNetPGP::pgp_add_subpacket(subkey, sigpkt)
+        ensure
+          LibNetPGP::pgp_create_sig_delete(sig) if sig
+          LibNetPGP::pgp_teardown_memory_write(sigoutput, mem_sig) if mem_sig
+        end
+      }
+      ret = LibNetPGP::pgp_write_xfer_pubkey(output, native, subkeysring, armored ? 1 : 0)
+      return nil if ret != 1
+      data = mem[:buf].read_bytes(mem[:length])
+      data
+    ensure
+      LibNetPGP::pgp_teardown_memory_write(output, mem) if mem
+    end
+   end
+
   def public_keys
     self.select {|key|
       key.is_a?(PublicKey)
