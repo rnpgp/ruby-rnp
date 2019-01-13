e699f66ebd2e4652c7b001154ef8aca7196892fc
Daniel Wyatt <daniel.wyatt@ribose.com>
2019-01-13 13:22:27 -0500

    Update Rnp::Verify test to check for per-signature-opts feature.
    This is NOTIMPL on older versions of librnp, so we need to put
    some conditions in this test or add a separate conditional test.

--- spec/sign/sign_spec.rb
+++ spec/sign/sign_spec.rb
@@ -38,11 +38,22 @@ describe Rnp::Verify do
     output = Rnp::Output.to_string
     sign = rnp.start_sign(input: Rnp::Input.from_string('data'),
                           output: output)
+    # these values won't be used, since they're modified later (before execute)
     sign.hash = 'SHA256'
     sign.expiration_time = 120
     sign.add_signer(rnp.find_key(userid: 'key0-uid1'))
-    sign.add_signer(rnp.find_key(userid: 'key1-uid2'), hash: 'SHA256',
-                    expiration_time: 120)
+    sign.add_signer(
+      rnp.find_key(userid: 'key1-uid2'),
+      if Rnp.has?('per-signature-opts')
+        {
+          hash: 'SHA256',
+          expiration_time: 120
+        }
+      else
+        {}
+      end
+    )
+    # these values will be used for sigs that do not explicitly set these
     sign.hash = 'SHA512'
     sign.expiration_time = 60
     rnp.password_provider = 'password'
@@ -65,10 +76,15 @@ describe Rnp::Verify do
     expect(sigs[0].valid?).to be true
     expect(sigs[0].expired?).to be false
 
-    expect(sigs[1].hash).to eql 'SHA256'
+    if Rnp.has?('per-signature-opts')
+      expect(sigs[1].hash).to eql 'SHA256'
+      expect(sigs[1].expiration_time).to eql 120
+    else
+      expect(sigs[1].hash).to eql 'SHA512'
+      expect(sigs[1].expiration_time).to eql 60
+    end
     expect(sigs[1].key.keyid).to eql '2FCADF05FFA501BB'
     expect(Time.now - sigs[0].creation_time).to be <= 5
-    expect(sigs[1].expiration_time).to eql 120
     expect(sigs[1].good?).to be true
     expect(sigs[1].valid?).to be true
     expect(sigs[1].expired?).to be false
