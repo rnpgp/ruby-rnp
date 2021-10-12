9f3eeb576396d61d128ff8bde58345204806c91a
Alexande B <abobrikovich@gmail.com>
2021-10-12 10:49:09 +0200

    simplify build workflow to reuse build scripts from rnp repo

--- ci/setup-env.inc.sh
+++
@@ -1,9 +0,0 @@
-set -euxo pipefail
-
-mkdir -p installs
-LOCAL_INSTALLS=/tmp/rnp-local-installs
-ln -s "$GITHUB_WORKSPACE/installs" /tmp/rnp-local-installs
-echo "CACHE_DIR=installs" >> $GITHUB_ENV
-echo "BOTAN_INSTALL=$GITHUB_WORKSPACE/installs/botan" >> $GITHUB_ENV
-echo "JSONC_INSTALL=$GITHUB_WORKSPACE/installs/jsonc" >> $GITHUB_ENV
-echo "RNP_INSTALL=$GITHUB_WORKSPACE/installs/rnp" >> $GITHUB_ENV
